a02cffe0d9e919715175c0d2eb573e7aee2adb93
ZH-HG <a58589961221@gmail.com>
2025-04-15 17:51:01 +0800

    update build.gradles

--- pax/build.gradle.kts
+++ pax/build.gradle.kts
@@ -1,19 +1,7 @@
 plugins {
-    id("java")
+    `java-library`
 }
-
-group = "com.hotsauce.payments.creditcard.castles"
-version = "unspecified"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
