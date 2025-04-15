92800f4854bb08a7d1ca3771acfc3bd4332e40a9
ZH-HG <a58589961221@gmail.com>
2025-04-15 13:00:01 +0800

    build.gradle add logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,21 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
+
             val org = System.getenv("GITHUB_ORG")
             val repo = System.getenv("GITHUB_REPO")
+            val actor = System.getenv("GITHUB_ACTOR")
+            val tokenSet = System.getenv("GITHUB_TOKEN") != null
+
             url = uri("https://maven.pkg.github.com/$org/$repo")
+
+            logger.lifecycle("Publishing to GitHub Packages:")
+            logger.lifecycle(" - url: $url")
+            logger.lifecycle(" - actor: $actor")
+            logger.lifecycle(" - token is set: $tokenSet")
+
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
+                username = actor
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
